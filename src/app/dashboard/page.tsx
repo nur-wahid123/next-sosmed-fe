4bb920aae4580c008a608efdca036270338e2b2a
nur-wahid123 <114736314+nur-wahid123@users.noreply.github.com>
2024-09-20 22:25:23 +0700

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function DashboardPage() {
   return (
     <div className='p-2 min-h-screen'>
-        <h1>Welcome to Dashboard Page</h1>
+        <h1>Welcome to Dashboard Page bro</h1>
     </div>
   )
 }
