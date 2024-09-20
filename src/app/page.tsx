75a5f944b93956b8af4aa1472827f88f97dfdf4d
Indra Fajar Nurwahid <fajarnurwahid99@gmail.com>
2024-09-20 16:02:57 +0700

    adsaj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,9 @@ export default function Home() {
           <div className="w-10/12 xl:w-8/12 pt-20 mx-auto">
             <div className="flex items-center justify-between font-medium">
               <div className="w-1/3">
-                <p className="text-xl">Sometimes Do Something Yourself is Difficult</p>
+                <p className="text-xl">
+                  Sometimes Does Something Yourself is Difficult
+                </p>
               </div>
               <div className="text-right">
                 <p>Our Point Of Sale is the best in the world</p>
